5d203cff7b801b82871ea93028cb06019fa2727d
Shun Yao <ysmiracle@hotmail.com>
2018-04-23 00:07:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -42,9 +42,8 @@ class Application
 
   def handle_cart_search(search_term)
     if @@items.include?(search_term)
-
-      "added Figs"
       @@cart << search_term
+      "added Figs"
     else
       "We don't have that item"
     end
